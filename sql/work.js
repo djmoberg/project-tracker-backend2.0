dddbe7401788b9cfb7db97cd38722f4e48d89fba
Daniel <daniel@moberg.no>
2018-09-15 20:07:40 +0200

    work.workFrom DESC

--- sql/work.js
+++ sql/work.js
@@ -1,7 +1,7 @@
 module.exports = {
     insert: "INSERT INTO work (user, project, workDate, workFrom, workTo, comment) VALUES (?)",
     select: {
-        work: "SELECT work.id, users.name, work.workDate, work.workFrom, work.workTo, work.comment FROM work INNER JOIN users ON users.id = work.user WHERE work.project = ? ORDER BY work.workDate DESC"
+        work: "SELECT work.id, users.name, work.workDate, work.workFrom, work.workTo, work.comment FROM work INNER JOIN users ON users.id = work.user WHERE work.project = ? ORDER BY work.workDate DESC, work.workFrom DESC"
     },
     update: {
         main: "UPDATE work SET workDate = ?, workFrom = ?, workTo = ?, comment = ? WHERE id = ? AND project = ?",
